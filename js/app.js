8428f2afcdeb7dae70f4c5cdc320ddafa54cf50c
Amarah Abusohyon <amarhbishtawi@gmail.com>
2021-03-01 17:13:27 +0200

    everything even if it hidden have a job , u can hid your necessary files from other eyes

---
+++ js/app.js
@@ -0,0 +1,54 @@
+'use strict';
+alert("HOW AM I ?")
+alert("MY name is Amarh abusohyon, i am a Biomedical Informatics Engineer, I am 24 years old \n right now i am taking a courses in software development, and i really love programming.")
+
+function wrongAnswer(answer, string) {
+    while (!(answer === 'Y') && !(answer === 'N') && !(answer === 'YES') && !(answer === 'NO')) {
+        alert("PLEASE, answer with y/n , yes/no ")
+        answer = prompt(string)
+
+        //console.log(!(answer === 'Y'))
+    }
+}
+
+
+
+alert("ANSWER the Questions with yes/no ,y/n")
+
+let ask01_Q = "MY NAME AMEERA ? 🤨"
+let ask01 = prompt(ask01_Q).toUpperCase()
+
+wrongAnswer(ask01, ask01_Q)
+console.log(ask01)
+document.getElementById("q1").innerText = ask01
+    ///////////////////////////////////////////////////////////////
+let ask02_Q = "MY age 42 🧐 "
+let ask02 = prompt(ask02_Q).toUpperCase()
+
+wrongAnswer(ask02, ask02_Q)
+
+if (ask02 === 'YES' || ask02 === 'Y') alert("you r blind 🤬")
+
+
+console.log(ask02)
+document.getElementById("q2").innerText = ask02
+    ///////////////////////////////////////////////////////////////
+let ask03_Q = "i love PROGRAMMING?🙄"
+let ask03 = prompt(ask03_Q).toUpperCase()
+
+wrongAnswer(ask03, ask03_Q)
+console.log(ask03)
+document.getElementById("q3").innerText = ask03
+    ///////////////////////////////////////////////////////////
+let ask04_Q = "I Studied a biology ?"
+let ask04 = prompt(ask04_Q).toUpperCase()
+wrongAnswer(ask04, ask04_Q)
+console.log(ask04)
+document.getElementById("q4").innerText = ask04
+
+///////////////////////////////////////////////////////////////////
+let ask05_Q = "MY favourite color is blue? 🤗 "
+let ask05 = prompt(ask05_Q).toUpperCase()
+wrongAnswer(ask05, ask05_Q)
+console.log(ask05)
+document.getElementById("q5").innerText = ask05
